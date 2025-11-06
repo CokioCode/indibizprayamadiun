ddbee3cef5aa96232eb44f2737bb24cbea9c67d4
abydanu <abydanuprasetyo@gmail.com>
2025-11-06 17:54:05 +0700

    fix: add file extension to import path for botController in bot routes

--- src/core/routes/bot.ts
+++ src/core/routes/bot.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import { botController } from "../../core/controllers/bot";
+import { botController } from "../../core/controllers/bot.js";
 
 const botRoutes = new Hono();
 
